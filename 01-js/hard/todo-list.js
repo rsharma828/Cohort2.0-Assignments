330231d67050a99d7032fc0780484e49ff78d328
Rupesh sharma <rksharmagmo@gmail.com>
2024-02-01 15:33:21 +0530

    Update todo-list.js
    added solution to this question

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,6 +11,36 @@
 */
 
 class Todo {
+  constructor(){
+    this.array = [];
+  }
+
+  add(todo){
+    this.array.push(todo);
+  }
+  remove(indexOfTodo){
+    if(indexOfTodo>=0 && indexOfTodo<this.array.length){
+      this.array.splice(indexOfTodo,1);
+    }
+  }
+  update(index,updatedTodo){
+    if(index>=0 && index<this.array.length){
+      this.array[index] = updatedTodo;
+    }
+  }
+  getAll(){
+    return this.array;
+  }
+  get(indexOfTodo){
+    
+    if(indexOfTodo>=0 && indexOfTodo<this.array.length){
+      return this.array[indexOfTodo];
+    }
+    return null;
+  }
+  clear(){
+    this.array.length = 0;
+  }
 
 }
 
