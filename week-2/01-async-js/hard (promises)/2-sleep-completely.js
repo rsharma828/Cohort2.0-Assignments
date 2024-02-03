9915e5e8721d6bdfbdf0ed843c712844325593f5
Rupesh sharma <rksharmagmo@gmail.com>
2024-02-03 17:03:56 +0530

    Update 2-sleep-completely.js - add solution to this question

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -5,6 +5,11 @@
  */
 
 function sleep(milliseconds) {
+  return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, milliseconds); // Convert seconds to milliseconds
+    })
 }
 
 module.exports = sleep;
